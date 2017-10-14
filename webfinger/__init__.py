0337d51dc2c65c376f30046a0869c6fabf012cd0
Elizabeth Myers <elizabeth@interlinked.me>
2017-10-13 21:55:30 -0500

    Improve docs and import WebFingerBuilder

--- webfinger/__init__.py
+++ webfinger/__init__.py
@@ -2,15 +2,29 @@
 
 WebFinger is a discovery protocol that allows you to find information about
 people or things in a standardized way.
+
+This package provides a few tools for using WebFinger, including:
+    - requests-based webfinger client (webfinger.client.requests.WebFingerClient)
+    - aiohttp-based webfinger client(webfinger.client.aiohttp.WebFingerClient)
+    - a class to build WebFinger JRD's (webfinger.objects.WebFingerBuilder)
+
+In this module, the following are exposed:
+    - BaseWebFingerClient (from webfinger.client)
+    - WebFingerClient (from webfinger.client.requests  for backwards
+      compatibility)
+    - The WebFingerResponse and WebFingerBuilder objects (from
+      webfinger.objects)
+    - Exceptions (from webfinger.exceptions)
+    - A simple helper for basic finger requests (the finger function)
 """
 
 
-__version__ = "3.0.0.dev0"
+__version__ = "3.0.0.dev1"
 
 
-# Backwards compatibility stubs
+from webfinger.client import BaseWebFingerClient
 from webfinger.client.requests import WebFingerClient
-from webfinger.objects import WebFingerResponse
+from webfinger.objects import WebFingerResponse, WebFingerBuilder
 from webfinger.exceptions import *
 
 
